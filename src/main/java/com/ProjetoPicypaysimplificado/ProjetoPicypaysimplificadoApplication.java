bfc12faff27b6bb6b57eb3c8215b31b6fdf814c9
Alatoride <iagon.araujo14@gmail.com>
2024-02-13 20:43:32 -0300

    feat:  initial config

---
+++ src/main/java/com/ProjetoPicypaysimplificado/ProjetoPicypaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoPicypaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPicypaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPicypaysimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjetoPicypaysimplificado/ProjetoPicypaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetoPicypaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPicypaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
